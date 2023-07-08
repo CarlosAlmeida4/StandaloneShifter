79a7ab2b7abaff78912356f876d1ed0ef47bd164
Carlos <ceduardosa4@gmail.com>
2023-07-08 20:19:26 +0100

    added screenControl logic

--- MDB/Startup.m
+++ MDB/Startup.m
@@ -1,3 +1,15 @@
 Ts = 0.001;
 Max_Gear = 5;
 AbsoluteMaxGear = 30;
+%                 1 2 3 4 5 6 7 8
+Display1stGear = [0,1,1,0,0,0,0,0];
+Display2ndGear = [1,1,1,1,1,0,1,0];
+Display3rdGear = [1,1,1,1,0,0,1,0];
+Display4thGear = [0,1,1,0,0,1,1,0];
+Display5thGear = [1,0,1,1,0,1,1,0];
+Display6thGear = [1,0,1,1,1,1,1,0];
+Display7thGear = [1,1,1,0,0,0,0,0];
+Display8thGear = [1,1,1,1,1,1,1,0];
+Display9thGear = [1,1,1,1,0,1,1,0];
+DisplayRGear   = [0,1,1,0,0,0,0,1];
+DisplayNGear   = [1,1,1,1,1,1,0,1];
